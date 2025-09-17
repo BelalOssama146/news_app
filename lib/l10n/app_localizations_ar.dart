643b2e121bf9fde311838a8eceeb5a639f6f31f0
Belal Osama <Belal.ossama2233@gmail.com>
2025-09-17 13:55:04 +0300

    I finished pagination and applied MVVM pattern with provider

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,55 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get news => 'الأخبار';
+
+  @override
+  String get title => 'عنوان الخبر';
+
+  @override
+  String get settings => 'العربية';
+
+  @override
+  String get categories => 'الفئات';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get pickCategory => 'اختر فئة اهتمامك';
+
+  @override
+  String get sports => 'الرياضة';
+
+  @override
+  String get technology => 'التكنولوجيا';
+
+  @override
+  String get health => 'الصحة';
+
+  @override
+  String get business => 'الأعمال';
+
+  @override
+  String get entertainment => 'الترفيه';
+
+  @override
+  String get science => 'العلوم';
+
+  @override
+  String get search => 'ابحث';
+
+  @override
+  String get wrong => 'هناك خطأ ما يرجى المحاولة لاحقا';
+
+  @override
+  String get view => 'عرض المقال بالكامل';
+}
